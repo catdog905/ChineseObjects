62ba0ddd7843e8901d91bcd56c0f3a11dbf56939
Yaroslav <y.kivaev@gmail.com>
2023-09-26 15:52:35 +0800

    Working calculator

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var calculator = new ChineseObjects.Calculator.CalculatorParser();
+calculator.Parse("(1 + 2) * 5");

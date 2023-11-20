b1ad4d29c0d067ca804ade137c504a9f6e1b68d7
Ghadeer <ghadeer_akleh@hotmail.com>
2023-11-20 07:42:20 +0300

    ScopeAware wrapper was implemented for While statement

---
+++ Lang/AbstractSyntaxTree/ScopeAwareTree/Statement/WhileStatement.cs
@@ -0,0 +1,49 @@
+namespace ChineseObjects.Lang;
+
+public interface IScopeAwareWhile : IWhileDeclaration, IScopeAwareStatementsBlock {}
+
+public class ScopeAwareWhile : IScopeAwareWhile
+{
+    private readonly Scope _scope;
+    private readonly IExpressionDeclaration _cond; // Should be ScopeAwareExpression?
+    private readonly IScopeAwareStatementsBlock _body;
+
+    public ScopeAwareWhile(Scope scope, IExpressionDeclaration condition, IScopeAwareStatementsBlock body)
+    {
+        _scope = scope;
+        _cond = condition;
+        _body = body;
+    }
+
+    public Scope Scope()
+    {
+        return _scope;
+    }
+
+    public IExpressionDeclaration Condition()
+    {
+        return _cond;
+    }
+
+    public IScopeAwareStatementsBlock Body()
+    {
+        return _body;
+    }
+
+    //TODO: Could be a need for a proper interface methods implementation 
+    public IEnumerable<IStatementDeclaration> Statements()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerable<IStatement> IStatementsBlock.Statements()
+    {
+        return Statements();
+    }
+
+    IList<string> IHumanReadable.GetRepr()
+    {
+        throw new NotImplementedException();
+    }
+
+}

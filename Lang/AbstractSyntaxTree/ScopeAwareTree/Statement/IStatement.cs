236d3f3450fa00458a90005e173150516ed5c3fb
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2023-11-25 15:44:49 +0800

    Implement `ScopeAwareStatementsBlock`

--- Lang/AbstractSyntaxTree/ScopeAwareTree/Statement/IStatement.cs
+++ Lang/AbstractSyntaxTree/ScopeAwareTree/Statement/IStatement.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+
 namespace ChineseObjects.Lang;
 
 public interface IScopeAwareStatement : IScopeAwareAstNode {}
@@ -7,17 +9,38 @@ public interface IScopeAwareStatementsBlock : IScopeAwareAstNode
     public IEnumerable<IScopeAwareStatement> Statements();
 }
 
-public class ScopeAwareStatementsBlock : IScopeAwareStatementsBlock {
+public class ScopeAwareStatementsBlock : IScopeAwareStatementsBlock
+{
+    private readonly Scope _scope;
+    private readonly IEnumerable<IScopeAwareStatement> _stmts;
+
+    public ScopeAwareStatementsBlock(Scope scope, IEnumerable<IStatement> stmts)
+    {
+        // Keep the original scope in the class, although more entities might be added to scope throughout the block 
+        _scope = scope;
+
+        Scope cur = scope;
+        var awareStmts = new List<IScopeAwareStatement>();
+        foreach (IStatement stmt in stmts)
+        {
+            IScopeAwareStatement aware = Irrealizable.MakeScopeAware(cur, stmt);
+            // `aware` might have introduced anything new into the scope:
+            cur = aware.Scope();
+            awareStmts.Add(aware);
+        }
+
+        _stmts = awareStmts.ToImmutableList();
+    }
     
-    public ScopeAwareStatementsBlock(Scope scope, IStatementsBlock body) {}
+    public ScopeAwareStatementsBlock(Scope scope, IStatementsBlock body) : this(scope, body.Statements()) {}
 
     public IEnumerable<IScopeAwareStatement> Statements()
     {
-        throw new NotImplementedException();
+        return _stmts;
     }
 
     public Scope Scope()
     {
-        throw new NotImplementedException();
+        return _scope;
     }
 }

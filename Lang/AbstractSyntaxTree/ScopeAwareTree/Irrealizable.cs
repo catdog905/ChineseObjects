05749ea80323084281ff24b039c7be87bd342532
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2023-11-25 13:59:17 +0800

    Add `Irrealizable` as a temporary solution for adding scope awareness to `IExpression` and `IStatement`

---
+++ Lang/AbstractSyntaxTree/ScopeAwareTree/Irrealizable.cs
@@ -0,0 +1,14 @@
+namespace ChineseObjects.Lang;
+
+public class Irrealizable
+{
+    public static IScopeAwareExpression MakeScopeAware(Scope scope, IExpression expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IScopeAwareStatement MakeScopeAware(Scope scope, IStatement statement)
+    {
+        throw new NotImplementedException();
+    }
+}
